748449cfabe6e9d0cc77cc23d9ba6f645e1309e2
CarlosGutierrezFerrira <80927233+CarlosGutierrezFerrira@users.noreply.github.com>
2021-06-05 20:13:05 -0400

    Delete script2.sh

--- script2.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash
-# Descarga y descomprime SRA Toolkit
-wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz
-tar -xzf sratoolkit.current-centos_linux64.tar.gz
